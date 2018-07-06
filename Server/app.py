96750b543d3fe816c6d5029335d097245a4b2dc0
KeepSorted <251010009@qq.com>
2018-07-06 12:12:09 +0800

    将REST.py中的内容整合到app.py中

--- Server/app.py
+++ Server/app.py
@@ -1,19 +1,62 @@
 import sys
-
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Resource,abort,reqparse
 import flask_restful as restful
-import os
-from REST import RESTExample,RESTNodeInfo
 import datetime
 import docker
-app = Flask(__name__)
+from flask import request,Flask,url_for,send_from_directory
+from werkzeug.security import safe_join as safe_join
+from datetime import date,timedelta,datetime
+import requests
+import re
+import json
+import redis
+import hashlib
+import os
 
+
+
+app = Flask(__name__)
 CORS(app) #跨域
-api = restful.Api(app)
+
+
+# get参数解析器
+parser=reqparse.RequestParser()
+parser.add_argument('type',type=str)
+
+# redis内存数据库用作进程通信
+sessionPool=redis.Redis(host='localhost',port=6379)#内存会话记录
+
+
+# ===========================================
+#测试
+class RESTExample(Resource):
+    def get(self):
+        return {'msg': 'ok example'}
+
+    def post(self):
+        abort(400) # 取消
+
+
+# 上传节点信息
+class RESTNodeInfo(Resource):
+    def get(self):
+        pass
+    
+    # 通过post上传节点信息，python中使用 request.post即可上传
+    def post(self):
+        id = request.form['id']
+        cpu = request.form['cpu']
+        memory = request.form['memory']
+        hdd = request.form['hdd']
+
+        print(cpu, memory, hdd)
+
+        return {'msg': 'success'}
 
 # ==================================================
+api = restful.Api(app)
 # 接口列表，将/example路由到RESTExample类
 api.add_resource(RESTExample, '/example')
 
